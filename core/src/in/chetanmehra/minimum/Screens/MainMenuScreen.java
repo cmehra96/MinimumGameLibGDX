e311b3b30725dbd6cedeb76a563c375615294382
cmehra <cmehra96@gmail.com>
2019-12-20 00:58:39 +0530

    Improve Main Menu UI.

--- core/src/in/chetanmehra/minimum/Screens/MainMenuScreen.java
+++ core/src/in/chetanmehra/minimum/Screens/MainMenuScreen.java
@@ -10,7 +10,9 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -27,8 +29,12 @@ public class MainMenuScreen extends AbstractScreen {
     private Button newComputerGame;
     private Button creditsScreen;
     private Button gameInstructionsBtn;
+    private Label mainTitle;
+    private Table titleTable;
+    private Table buttonTable;
     private Skin skin;
     GestureDetector gestureDetector;
+
 
 
     public MainMenuScreen(Assests assests) {
@@ -40,16 +46,41 @@ public class MainMenuScreen extends AbstractScreen {
         stage = new Stage(viewport);
         stage2 = new Stage(viewport);
         Gdx.input.setCatchKey(Input.Keys.BACK, true);
+        skin = assests.manager.get(Assests.glassySkin);
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(stage);
+        multiplexer.addProcessor(stage2);
+
+        gestureDetector = new GestureDetector(this);
+        gestureDetector.setLongPressSeconds(0.5f);  // long press set to half second
+        multiplexer.addProcessor(gestureDetector);
+        Gdx.input.setInputProcessor(multiplexer);
+        addTitle();
+        addButtons();
+
 
     }
 
-    public void show() {
-        //  Gdx.app.log(TAG, "Enters show method");
+    private void addTitle() {
         Image backgroundImage = new Image(assests.manager.get(Assests.backgroundImageTexture));
         backgroundImage.setSize(width, height);
         stage.addActor(backgroundImage);
-        skin = assests.manager.get(Assests.neonSkin);
-        TextButtonStyle textButtonStyle = new TextButtonStyle();
+        mainTitle = new Label("Minimum Game", skin);
+        mainTitle.setFontScale(4);
+        titleTable = new Table();
+        titleTable.setFillParent(true);
+        titleTable.top();
+        titleTable.add(mainTitle).expandX().padTop(0.05f * height);
+        stage.addActor(titleTable);
+
+    }
+/*
+    public void show() {
+        //  Gdx.app.log(TAG, "Enters show method");
+
+        addTitle();
+        addButtons();
+
         newComputerGame = new TextButton("Single Player", skin);
         newComputerGame.setSize(230.0f, 95.0f);
         newComputerGame.setPosition(width / 2, height / 4);
@@ -59,15 +90,26 @@ public class MainMenuScreen extends AbstractScreen {
         stage.addActor(newComputerGame);
         stage.addActor(creditsScreen);
         LoadButtonListeners();
-        InputMultiplexer multiplexer = new InputMultiplexer();
-        multiplexer.addProcessor(stage);
-        multiplexer.addProcessor(stage2);
-        gestureDetector = new GestureDetector(this);
-        gestureDetector.setLongPressSeconds(0.5f);  // long press set to half second
-        multiplexer.addProcessor(gestureDetector);
-        Gdx.input.setInputProcessor(multiplexer);
+
+
 
         //Gdx.app.log(TAG, "Executed show method succussfully");
+    }
+    */
+
+    private void addButtons() {
+        buttonTable = new Table();
+        buttonTable.center();
+        buttonTable.setFillParent(true);
+        newComputerGame = new TextButton("Single Player", skin);
+        creditsScreen = new TextButton("Credits", skin);
+        buttonTable.add(newComputerGame).center().padBottom(40);
+        buttonTable.row();
+        buttonTable.add(creditsScreen).center().padBottom(40);
+        buttonTable.row();
+        stage.addActor(buttonTable);
+        LoadButtonListeners();
+
     }
 
 
